9eec2b418debd9eca4e08f6a9ad02a1411fdc95a
PCSang <88499749+pcsang@users.noreply.github.com>
2022-03-30 23:28:02 +0700

    update controll with authentication

--- src/main/java/com/example/webclient/controll/Controller.java
+++ src/main/java/com/example/webclient/controll/Controller.java
@@ -25,34 +25,46 @@ public class Controller {
 
     @GetMapping("/books")
     public Flux<Book> getBooks(){
-        return webClient.get().uri("").retrieve().bodyToFlux(Book.class);
+        return webClient.get().uri("")
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .retrieve().bodyToFlux(Book.class);
     }
 
     @GetMapping("/book/{id}")
     public Mono<Book> getBookById(@PathVariable int id){
-        return webClient.get().uri("/"+id).retrieve().bodyToMono(Book.class);
+        return webClient.get().uri("/"+id)
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .retrieve().bodyToMono(Book.class);
     }
 
     @GetMapping(path = "/books_author_category")
     public Flux<Book> getBookByAuthor_Category(@RequestParam(required = false) String author,
                                                @RequestParam(required = false) String category){
-        return webClient.get().uri("/test?author="+author+"&category="+category).retrieve().bodyToFlux(Book.class);
+        return webClient.get().uri("/test?author="+author+"&category="+category)
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .retrieve().bodyToFlux(Book.class);
     }
 
     @PostMapping("/book")
     public Mono<Book> addBookNew(@RequestBody Book book){
-        return webClient.post().uri("").syncBody(book).retrieve().bodyToMono(Book.class);
+        return webClient.post().uri("")
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .syncBody(book).retrieve().bodyToMono(Book.class);
     }
 
     @DeleteMapping(path = "/deletebook/{id}")
     public Mono<Book> deteleBook(@PathVariable int id){
-        return webClient.delete().uri("/"+id).retrieve().bodyToMono(Book.class);
+        return webClient.delete().uri("/"+id)
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .retrieve().bodyToMono(Book.class);
     }
 
     @PutMapping(path = "/changebook/{id}")
     public Mono<Book> updateBookById(@PathVariable int id,
                                      @RequestParam(required = false) String name,
                                      @RequestParam(required = false) String author){
-        return webClient.put().uri("/"+id+"?"+"name="+name+"&author="+author).retrieve().bodyToMono(Book.class);
+        return webClient.put().uri("/"+id+"?"+"name="+name+"&author="+author)
+                .headers(HttpHeaders->HttpHeaders.setBasicAuth("sang", "sangpass"))
+                .retrieve().bodyToMono(Book.class);
     }
 }
